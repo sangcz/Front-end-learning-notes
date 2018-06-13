1a177ffefcc756cd0c4e23f94371809b7c8f028b
桑朝忠 <sangchaozhong@xcjrbank.com>
2018-06-13 11:41:19 +0800

    updated config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
     base: '/Front-end-learning-notes/',
     dest: '.vuepress/dist',
-=======
-    base: '/',
->>>>>>> fa9e1db6684d460375f362e9fb8496c1081d70cf
     title: '前端学习笔记',
     description: 'Front-end learning notes',
     repo: 'https://sangcz.github.io/Front-end-learning-notes',
